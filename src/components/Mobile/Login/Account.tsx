98290bd1605eaeefa50178db041952dff56d629e
sem201 <sem201@naver.com>
2024-12-05 13:06:21 +0900

    [feat] 모바일/ 데스크탑 화면 구분

--- src/components/Mobile/Login/Account.tsx
+++ src/components/Mobile/Login/Account.tsx
@@ -6,12 +6,12 @@ const Account = () => {
     window.location.href = KAKAO_AUTH_URI;
   };
   return (
-    <div className="border-2 w-[100px] bg-white border-[#C8AA6E] rounded-[15px] bg-opacity-30">
+    <div className="border-2 w-[100px] h-[200px] bg-white border-[#C8AA6E] rounded-[15px] bg-opacity-30">
       <div className="flex flex-col justify-center items-center">
         <div>Login</div>
         <button
           onClick={loginHandler}
-          className=" bg-[url(./assets/kakao_login_small.png)] bg-no-repeat bg-cover"
+          className=" bg-[url(./assets/kakao_login_small.png)] bg-no-repeat bg-full"
         ></button>
       </div>
     </div>
