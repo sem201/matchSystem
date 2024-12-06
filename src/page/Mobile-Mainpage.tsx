9d18069d2de71a9b0cd2ce5fe116ef73d24bafe3
sem201 <sem201@naver.com>
2024-12-06 13:09:19 +0900

    [design] user 정보 디자인 완성/ [design] line 선택 모달창 완성

--- src/page/Mobile-Mainpage.tsx
+++ src/page/Mobile-Mainpage.tsx
@@ -1,10 +1,25 @@
 import TeamContainer from "../components/Mobile/Main/TeamContainer";
+import UserInfo from "../components/Mobile/chooseUser/UserInfo";
+import Container from "../components/Mobile/common/Container";
 
 const MobileMainpage = () => {
   return (
-    <div className="flex flex-col items-center justify-center w-[100vw] h-[100vh]">
+    <div className="flex flex-col items-center justify-start w-[100vw] max-h-[100vh] overflow-y-scroll">
       <TeamContainer color="RED" />
       <TeamContainer color="BLUE" />
+
+      <Container>
+        <UserInfo />
+        <UserInfo />
+        <UserInfo />
+        <UserInfo />
+        <UserInfo />
+        <UserInfo />
+        <UserInfo />
+        <UserInfo />
+        <UserInfo />
+        <UserInfo />
+      </Container>
     </div>
   );
 };
