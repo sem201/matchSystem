ba18fe4a1b28e8c8be120954af2d83d8cb9648bc
LeeYongSeok13 <minamiharuka333@gmail.com>
2024-12-06 13:07:11 +0900

    design login finish main dev-ing

---
+++ src/components/Desktop/Versus.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Versus = () => {
+  return (
+    <div className="text-[2.5vw] font-bold text-center flex gap-[5%] justify-center">
+      <span className="text-[#8A2922]">Red Team</span>
+      <span className="text-white">VS</span>
+      <span className="text-[#004A82]">Blue Team</span>
+    </div>
+  );
+};
+
+export default Versus;
