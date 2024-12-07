107dc39420140fee2a763614cc9f3de7ec2223cc
kimminjeong2 <zbdj0123@naver.com>
2024-12-07 12:49:56 +0900

    모바일 메인페이지 상세수정

--- client/src/components/Mobile/Main/TeamContainer.tsx
+++ client/src/components/Mobile/Main/TeamContainer.tsx
@@ -1,15 +1,30 @@
 import React from "react";
+
 interface Props {
   color: string;
 }
+
 const TeamContainer: React.FC<Props> = ({ color }) => {
   const textColor = color === "RED" ? "#8A2922" : "#004A82";
   return (
     <>
-      <p className="text-[30px]" style={{ color: textColor }}>
+      <p
+        className="text-[30px] font-blackHanSans"
+        style={{
+          color: textColor,
+          WebkitTextStroke: "1px #C8AA6E",
+        }}
+      >
         {color} TEAM
       </p>
-      <div className="bg-[#F0E6D2] border-2 border-[#C8AA6E] rounded-[15px] bg-opacity-15 w-[300px] min-h-[250px]"></div>
+      <div className="bg-[#F0E6D2] border-2 border-[#C8AA6E] rounded-[40px] bg-opacity-15 w-[300px] min-h-[250px] mb-7">
+        <div className="flex flex-col justify-between h-full space-y-12 mt-12">
+          <div className="h-[1px] bg-[#ffffff] bg-opacity-30 w-[80%] mx-auto"></div>
+          <div className="h-[1px] bg-[#ffffff] bg-opacity-30 w-[80%] mx-auto"></div>
+          <div className="h-[1px] bg-[#ffffff] bg-opacity-30 w-[80%] mx-auto"></div>
+          <div className="h-[1px] bg-[#ffffff] bg-opacity-30 w-[80%] mx-auto"></div>
+        </div>
+      </div>
     </>
   );
 };
--- client/src/components/Mobile/chooseUser/UserContainer.tsx
+++ client/src/components/Mobile/chooseUser/UserContainer.tsx
@@ -4,18 +4,8 @@ import UserInfo from "./UserInfo";
 const UserContainer = ({ children }: Props) => {
   return (
     <>
-      <div className="w-[300px] bg-white border-2 border-[#C8AA6E] rounded-[15px] bg-opacity-15 mt-10">
-        <div className="flex flex-row items-center justify-center space-x-4 w-full mb-6 mt-5">
-          <button className="w-[18vw] h-[4.5vh] bg-[#F0E6D2] rounded-full border-2 border-[#C8AA6E] text-[9px] text-[#0F2041] flex items-center justify-center font-black">
-            인원추가
-          </button>
-          <button className="w-[18vw] h-[4.5vh] bg-[#F0E6D2] rounded-full border-2 border-[#C8AA6E] text-[9px] text-[#0F2041] flex items-center justify-center font-extrabold">
-            모드선택
-          </button>
-          <button className="w-[18vw] h-[4.5vh] bg-[#F0E6D2] rounded-full border-2 border-[#C8AA6E] text-[9px] text-[#0F2041] flex items-center justify-center font-extrabold">
-            사용법
-          </button>
-        </div>
+      <div className="w-[300px] bg-[#F0E6D2] border-2 border-[#C8AA6E] rounded-[40px] bg-opacity-15 mt-7 mb-7 p-4">
+        <div className="ml-3 mb-5 mt-3">최근에 같이한 친구</div>
         <UserInfo />
         <UserInfo />
         <UserInfo />
@@ -28,9 +18,6 @@ const UserContainer = ({ children }: Props) => {
         <UserInfo />
         {children}
       </div>
-      <button className="w-[18vw] h-[4.5vh] bg-[#F0E6D2] rounded-full border-2 border-[#C8AA6E] text-[9px] text-[#0F2041] flex items-center justify-center font-extrabold mt-7 mb-7">
-        팀짜기
-      </button>
     </>
   );
 };
