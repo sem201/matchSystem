0975381259a1a85248686cdfda731b3fcc8cfb6b
LeeYongSeok13 <minamiharuka333@gmail.com>
2024-12-09 13:46:06 +0900

    mainpage design 수정 2일차(이용석)

--- client/src/page/Desktop-MainPage.tsx
+++ client/src/page/Desktop-MainPage.tsx
@@ -1,12 +1,14 @@
 import Team from "../components/Desktop/Team";
 import Header from "../components/Desktop/Header";
+import Menu from "../components/Desktop/Menu";
 
 const DesktopMainPage = () => {
   return (
     <div className="w-[100vw] h-[100vh]">
-      <div className="px-[50px]">
-        <Header />
+      <Header />
+      <div className="lg:grid lg:grid-rows-3 lg:grid-cols-3 gap-6 lg:w-[100%] lg:h-[70%] px-[50px]">
         <Team />
+        <Menu />
       </div>
     </div>
   );
