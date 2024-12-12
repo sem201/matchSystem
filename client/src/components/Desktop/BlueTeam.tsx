c7f7dd0dbbd316ddb8cc77959faaa30528054ea3
LeeYongSeok13 <minamiharuka333@gmail.com>
2024-12-12 10:36:15 +0900

    [design] 반응형 및 Team크기 조절

--- client/src/components/Desktop/BlueTeam.tsx
+++ client/src/components/Desktop/BlueTeam.tsx
@@ -22,33 +22,31 @@ const BlueTeam: React.FC<BlueTeamProps> = ({ user }) => {
     <div
       className={`w-[18%] max-h-[100%] border-2 border-solid border-[#004A82] rounded-2xl flex flex-col items-center p-2 ${backgroundClass}`}
     >
-      <div className="w-[100%]">
-        <ul>
-          {/* 유저 정보가 있을 경우 */}
-          {user ? (
-            <li
-              key={user.id}
-              className="flex flex-col items-center justify-between"
-            >
-              <img
-                src={line}
-                alt="라인 이미지"
-                className="w-5 h-5"
-                onClick={() => setIsLine(!isLine)}
-              />
-              <p className="text-xs font-bold">{user.nickname}</p>
-              <img src={tier} alt="tier" className="h-9" />
-              {isLine && <LineModal setLine={setLine} setIsLine={setIsLine} />}
-              <img src={close} alt="close" />
-            </li>
-          ) : (
-            // 유저 정보가 없을 경우
-            <li className="text-center text-gray-500">
-              <img src={PlusIcon} alt="Add" className="" />
-            </li>
-          )}
-        </ul>
-      </div>
+      <ul className="flex flex-col items-center justify-center h-full">
+        {/* 유저 정보가 있을 경우 */}
+        {user ? (
+          <li
+            key={user.id}
+            className="flex flex-col items-center justify-between h-full"
+          >
+            <img
+              src={line}
+              alt="라인 이미지"
+              className="w-5 h-5"
+              onClick={() => setIsLine(!isLine)}
+            />
+            <p className="text-xs font-bold">{user.nickname}</p>
+            <img src={tier} alt="tier" className="h-9" />
+            {isLine && <LineModal setLine={setLine} setIsLine={setIsLine} />}
+            <img src={close} alt="close" />
+          </li>
+        ) : (
+          // 유저 정보가 없을 경우
+          <li className="flex flex-col items-center justify-center text-gray-500 xs:h-[20vh] lg:h-full">
+            <img src={PlusIcon} alt="Add" className="" />
+          </li>
+        )}
+      </ul>
     </div>
   );
 };
--- client/src/page/Desktop-MainPage.tsx
+++ client/src/page/Desktop-MainPage.tsx
@@ -4,15 +4,13 @@ import Menu from "../components/Desktop/Menu";
 import { useState } from "react";
 
 import { User } from "../commonTypes";
+import ComposeButton from "../components/Desktop/ComposeButton";
 
 interface DesktopMainPageProps {
   allUser: User[];
   redTeam: User[];
   blueTeam: User[];
   onAddUserToTeam: (userId: number) => void;
-  setAllUser: React.Dispatch<React.SetStateAction<User[]>>;
-  setRedTeam: React.Dispatch<React.SetStateAction<User[]>>;
-  setBlueTeam: React.Dispatch<React.SetStateAction<User[]>>;
 }
 
 const DesktopMainPage: React.FC<DesktopMainPageProps> = ({
@@ -20,41 +18,21 @@ const DesktopMainPage: React.FC<DesktopMainPageProps> = ({
   redTeam,
   blueTeam,
   onAddUserToTeam,
-  setAllUser,
-  setRedTeam,
-  setBlueTeam,
 }) => {
   const [headerText, setHeaderText] = useState<string>("모드를 선택해주세요");
 
-  const handleRemoveUser = (userId: number) => {
-    const selectedUser = redTeam.find((user) => user.id === userId);
-    if (selectedUser) {
-      // user를 allUser로 다시 추가
-      setAllUser([...allUser, selectedUser]);
-
-      // redTeam, blueTeam에서 해당 유저 제거
-      setRedTeam(redTeam.filter((user) => user.id !== userId));
-      setBlueTeam(blueTeam.filter((user) => user.id !== userId));
-    }
-  };
   return (
     <div className="w-[100vw] xs:h-[100%] lg:h-[100vh]">
       <Header text={headerText} />
       <div className="lg:grid lg:grid-rows-3 lg:grid-cols-3 gap-6 lg:w-[100%] lg:h-[70%] px-[50px]">
-        <Team
-          redTeam={redTeam}
-          blueTeam={blueTeam}
-          onRemoveUser={handleRemoveUser}
-        />
+        <Team redTeam={redTeam} blueTeam={blueTeam} />
         <Menu
           setHeaderText={setHeaderText}
           allUser={allUser}
           onAddUserToTeam={onAddUserToTeam}
-          setAllUser={setAllUser}
-          setRedTeam={setRedTeam}
-          setBlueTeam={setBlueTeam}
         />
       </div>
+      <ComposeButton />
     </div>
   );
 };
