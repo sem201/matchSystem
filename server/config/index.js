1ad6a17d1d9b24f534dbb8b7ae8b1f738378a07b
echong22 <tdc8165@naver.com>
2024-12-13 13:20:58 +0900

    사용자 추가 api 구현중

--- server/config/index.js
+++ server/config/index.js
@@ -1,7 +1,7 @@
-import sequelize from "./config/db.js";
 import NoobsUserInfo from "../models/Noobs_user_info.js";
 import NoobsMasterChamp from "../models/Noobs_master_champ.js";
-
+import NoobsRecentFriends from "../models/Noobs_Recent_Friend.js";
+import Users from "../models/User.js";
 
 NoobsUserInfo.hasMany(NoobsMasterChamp, {
   foreignKey : 'user_id',
@@ -12,4 +12,15 @@ NoobsUserInfo.hasMany(NoobsMasterChamp, {
 NoobsMasterChamp.belongsTo(NoobsUserInfo, {
   foreignKey: 'user_id',
   targetKey: 'id',
+});
+
+
+Users.hasMany(NoobsRecentFriends, {
+  foreignKey: 'user_id', // Users.id와 연결
+  as: 'AddedFriends'     // 별칭: 사용자가 추가한 친구들
+});
+
+NoobsRecentFriends.belongsTo(Users, {
+  foreignKey: 'id', // Friends.friend_id는 Users.id를 참조
+  as: 'FriendDetails'      // 별칭: 친구의 상세정보
 });
--- server/routes/route.js
+++ server/routes/route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { ChampionUpdate, profileUpdate } from  '../controllers/updateController.js'; // 확장자 .js 추가
-import { userSearch } from '../controllers/userInfoController.js'
+import { userSearch, userAdd } from '../controllers/userInfoController.js'
 
 const router = express.Router();
 
@@ -12,5 +12,7 @@ router.post('/noobs/profileUpadate', profileUpdate);
 // 유저 검색 
 router.get('/noobs/lolUser', userSearch);
 
+// 유저추가 [ 최근에 같이한 플레이어 ]
+router.post('/noobs/lolUserAdd', userAdd)
 
 export default router; // ESM 방식으로 export
