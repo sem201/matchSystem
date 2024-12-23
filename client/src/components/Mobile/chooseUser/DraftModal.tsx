f6f799c40dcee0675f1034f4779b5fd35b6e8ee7
sem201 <sem201@naver.com>
2024-12-23 12:21:08 +0900

    [release] 불필요 콘솔 삭제

--- client/src/components/Mobile/chooseUser/DraftModal.tsx
+++ client/src/components/Mobile/chooseUser/DraftModal.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import DraftModal2 from "./DraftModal2";
 import { User } from "../../../commonTypes";
 import apiCall from "../../../Api/Api";
