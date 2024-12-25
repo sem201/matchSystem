0c5e516e5994b9d0529262a429fb48c7603b35f6
Ubuntu <ubuntu@ip-172-31-47-100.ap-northeast-2.compute.internal>
2024-12-25 15:28:35 +0000

    fdsfdsf

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ app.use(
 app.use(
   cors({
     // origin: "http://127.0.0.1:5173", // 요청을 허용할 클라이언트 도메인
-    origin: "http://15.165.204.99:5173", // 요청을 허용할 클라이언트 도메인
+    origin: "https://www.noobsapp.store", // 요청을 허용할 클라이언트 도메인
     credentials: true,
     methods: ["GET", "POST", "UPDATE", "PATCH"], // 허용할 HTTP 메서드
   })
@@ -39,6 +39,7 @@ app.use(
 
 // Preflight 요청에 대한 처리
 app.options("*", cors());
+
 
 app.use((req, res, next) => {
   next();
