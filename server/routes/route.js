7fc62bdf69d221384964336646f29aec2bdb9995
echong22 <tdc8165@naver.com>
2024-12-26 00:37:33 +0900

    api 수정

--- server/routes/route.js
+++ server/routes/route.js
@@ -27,44 +27,44 @@ const limiter = rateLimit({
 const router = express.Router();
 
 // 카카오 로그인 처리
-router.get('/user/kakao/login', kakaoLogin);
+router.get('api/user/kakao/login', kakaoLogin);
 
 // 로그아웃
-router.get('/logout', logout);
+router.get('api/logout', logout);
 // 샘플계정
-router.get('/passlogin', passlogin);
+router.get('api/passlogin', passlogin);
 
 // Noobs 사용자
-router.get('/noobs/nobsinfo', nobsinfo);
+router.get('api/noobs/nobsinfo', nobsinfo);
 
 
 // 챔피언 업데이트
-router.post("/noobs/championUpadate", ChampionUpdate);
+router.post("api/noobs/championUpadate", ChampionUpdate);
 // 사용자 프로필 정보 업데이트
-router.post("/noobs/profileUpadate", profileUpdate);
+router.post("api/noobs/profileUpadate", profileUpdate);
 
 // 유저 검색
-router.get("/noobs/lolUser", userSearch);
+router.get("api/noobs/lolUser", userSearch);
 
 // 유저추가 [ 최근에 같이한 플레이어 ]
-router.post("/noobs/lolUserAdd", userAdd);
+router.post("api/noobs/lolUserAdd", userAdd);
 
 // 추가한 유저들 가져오기
-router.get("/noobs/friendUserBr", friendUserBr);
+router.get("api/noobs/friendUserBr", friendUserBr);
 
 // 모드별 팀섞기
-router.post("/noobs/TeamMach", TeamMach);
+router.post("api/noobs/TeamMach", TeamMach);
 
 // 사용자 정보 업데이트
-router.post("/noobs/friendUserBrUpdate", friendUserBrUpdate);
+router.post("api/noobs/friendUserBrUpdate", friendUserBrUpdate);
 
 // 유저 삭제 [ 초근에 같이한 플에이어 ]
-router.post("/noobs/friendUserBrDel", friendUserBrDel);
+router.post("api/noobs/friendUserBrDel", friendUserBrDel);
 
 // 유저 포로필정보 요청
-router.post("/noobs/UserDetilsInfo", UserDetilsInfo);
+router.post("api/noobs/UserDetilsInfo", UserDetilsInfo);
 
-router.get("/sampleData", sampleData);
+router.get("api/sampleData", sampleData);
 
 
 
