d9961c88294793f16a5ae10533717bf5ff8b3f4a
echong22 <tdc8165@naver.com>
2024-12-26 09:07:22 +0900

    fdsfs

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use(
     rolling: false,
     cookie: {
       httpOnly: true,
-      sameSite: "None",
+      sameSite: "Lax",
       secure: true, // 개발 중에는 false로 설정 (HTTPS에서만 true)
       maxAge: 30 * 60 * 1000, // 세션 만료 10분
     },
